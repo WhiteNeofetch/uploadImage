42f876761873d477fe773e5d2c4b15a9b798efe0
WhiteNeofetch <rumordek@gmail.com>
2021-12-15 02:58:07 +0300

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,84 +1,89 @@
 <template>
-  <div id="my-strictly-unique-vue-upload-multiple-image" style="display: flex; justify-content: center;">
-    <vue-upload-multiple-image
-      dragText="Загрузите фото"
-      browseText="Загрузите фото1"
-      primaryText="Загрузите фото2"
-      markIsPrimaryText="Загрузите фото3"
-      dropText="Загрузите фото4"
-      popupText="Загрузите фото5"
-      accept="Загрузите фото6"
-      @upload-success="uploadImageSuccess"
-      @before-remove="beforeRemove"
-      @edit-image="editImage"
-      :data-images="images"
-      idUpload="myIdUpload"
-      editUpload="myIdEdit"
+  <div>
+    <div
+      id="my-strictly-unique-vue-upload-multiple-image"
+      style="display: flex; justify-content: center"
+    >
+      <vue-upload-multiple-image
+        dragText="Загрузите фото"
+        browseText="Загрузите фото1"
+        primaryText="Загрузите фото2"
+        markIsPrimaryText="Загрузите фото3"
+        dropText="Загрузите фото4"
+        popupText="Загрузите фото5"
+        accept="Загрузите фото6"
+        @upload-success="uploadImageSuccess"
+        @before-remove="beforeRemove"
+        @edit-image="editImage"
+        :data-images="images"
+        idUpload="myIdUpload"
+        editUpload="myIdEdit"
       ></vue-upload-multiple-image>
+    </div>
   </div>
 </template>
- 
-<script>
-import VueUploadMultipleImage from 'vue-upload-multiple-image'
-// import axios from 'axios'
-export default {
-  name: 'app',
-  data () {
-    return {
-      images: []
-    }
-  },
-  components: {
-    VueUploadMultipleImage
-  },
-  methods: {
-    uploadImageSuccess(formData, index, fileList) {
-      console.log('data', formData, index, fileList)
-      // Upload image api
-      // axios.post('http://your-url-upload', formData).then(response => {
-      //   console.log(response)
-      // })
-    },
-    beforeRemove (index, done, fileList) {
-      console.log('index', index, fileList)
-      var r = confirm("remove image")
-      if (r == true) {
-        done()
-      }
-    },
-    editImage (formData, index, fileList) {
-      console.log('edit data', formData, index, fileList)
-    }
-  }
-}
-</script> 
- 
+
 <style>
 #my-strictly-unique-vue-upload-multiple-image {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
- 
-h1, h2 {
+
+h1,
+h2 {
   font-weight: normal;
 }
- 
+
 ul {
   list-style-type: none;
   padding: 0;
 }
- 
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
- 
+
 a {
   color: #42b983;
 }
-</style> 
- 
+</style>
+
+<script>
+import VueUploadMultipleImage from "vue-upload-multiple-image";
+// import axios from 'axios'
+export default {
+  name: "app",
+  data() {
+    return {
+      images: [],
+    };
+  },
+  components: {
+    VueUploadMultipleImage,
+  },
+  methods: {
+    uploadImageSuccess(formData, index, fileList) {
+      console.log("data", formData, index, fileList);
+      // Upload image api
+      // axios.post('http://your-url-upload', formData).then(response => {
+      //   console.log(response)
+      // })
+    },
+    beforeRemove(index, done, fileList) {
+      console.log("index", index, fileList);
+      var r = confirm("remove image");
+      if (r == true) {
+        done();
+      }
+    },
+    editImage(formData, index, fileList) {
+      console.log("edit data", formData, index, fileList);
+    },
+  },
+};
+</script>
